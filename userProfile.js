1c4eb1559e5f5cc759930791973230bdb4f12b68
Mark-Joseph-Quizzagan <quizzaganmarkjoseph347@gmail.com>
2025-03-21 22:29:37 +0800

    Add files via upload

---
+++ userProfile.js
@@ -0,0 +1,11 @@
+// Collecting user information using prompt()
+let fullName = prompt("Please enter your full name (first and last name):");  //It will ask the user for his/her full name
+let age = prompt("Please enter your age:");  //It will ask the user for their age
+let favoriteNumber = prompt("Please enter your favorite number:");  //It will ask the user for their favorite number
+let favoriteColor = prompt("Please enter your favorite color:");  // It will ask the usertheir favorite color
+
+// Log the collected information in the console
+console.log("Full Name:", fullName);  // Outputting the full name in the console
+console.log("Age:", age);  // Outputting the age in the console
+console.log("Favorite Number:", favoriteNumber);  // Outputting the favorite number in the console
+console.log("Favorite Color:", favoriteColor);  // Outputting the favorite color in the console
